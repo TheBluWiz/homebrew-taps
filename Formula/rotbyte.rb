cf97baaa144a839c790c3c67bdff5cdf9efdf576
Jamey Wicklund <thebluwiz@icloud.com>
2026-04-18 15:01:41 -0700

    Fixes rotbyte SHA checksum

--- Formula/rotbyte.rb
+++ Formula/rotbyte.rb
@@ -2,7 +2,7 @@ class Rotbyte < Formula
   desc "Guard your files against silent data corruption (bit rot)"
   homepage "https://github.com/TheBluWiz/RotByte"
   url "https://github.com/TheBluWiz/RotByte/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "3172fec5a68891ab06c4f9de863ae77a25647de30bf66c2faad89ae78f78a712"
   license "MIT"
 
   depends_on "python@3.14"
